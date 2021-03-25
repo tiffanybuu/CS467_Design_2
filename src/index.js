8014dbe74fd33d51174c4bd79ba585635966fc71
tiffanybuu <tiffbuu@gmail.com>
2021-03-25 16:34:50 -0500

    reorder social capital rank, add ranking values to json, add social capital legend

--- src/index.js
+++ src/index.js
@@ -14,35 +14,42 @@ const height = svgHeight - padding.top - padding.bottom;
 const width = svgWidth - padding.right - padding.left;
 
 window.addEventListener("load", drawCircles);
-const svg = d3.select("svg")
+const svg = d3.select(".map")
 .attr('width', svgWidth)
 .attr('height', svgHeight);
 
-
   // Define path generator
 const path = d3.geoPath() // path generator that will convert GeoJSON to SVG paths
+const legendText = ["b", "o", "o", "p", "s"]
 
 function drawCircles() {
 
     d3.csv("social-capital-states.csv").then(function(data) {
 
-      data.forEach(function (d) {
-        d.State_Level_Index = parseFloat(d.State_Level_Index)
+      data.forEach(function (d, i) {
+        // console.log(i)
+        d.State_Level_Index = parseFloat(d.State_Level_Index);
+        d.rank_percentage = i/50.00;
+        // rank out of 50 states plus DC = 51
+        d.rank = i+1;
+        console.log(d.rank)
       })
 
       const index_range = d3.extent(data.map(d => d.State_Level_Index))
+      // const color = d3.scaleOrdinal().domain(index_range).range(["#E4E5A6", "#91E38E", "#36ADAB", "#0066ff", "#000099"])
       const color = d3.scaleQuantize().domain(index_range).range(["#000099", "#0066ff", "#36ADAB", "#91E38E", "#E4E5A6"])
 
+      const legend_color = ["#000099", "#0066ff", "#36ADAB", "#91E38E", "#E4E5A6"]; 
       d3.json("states-albers-10m.json").then(function(us) {
         const feat = topojson.feature(us, us.objects.states).features;
         // https://bl.ocks.org/wboykinm/dbbe50d1023f90d4e241712395c27fb3
         for (var i = 0; i < data.length; i++) {
 
-          var dataState = data[i].State;
-          var dataValue = parseFloat(data[i].State_Level_Index);
+          const dataState = data[i].State;
+          const dataValue = parseFloat(data[i].State_Level_Index);
           
           for (var j = 0; j < feat.length; j++)  {
-            var jsonState = feat[j].properties.name;
+            const jsonState = feat[j].properties.name;
 
             if (dataState == jsonState) {
             feat[j].properties.social_index = dataValue; 
@@ -68,6 +75,42 @@ function drawCircles() {
             .attr("d", path);
 
         });
+
+        // legend code found here: http://bl.ocks.org/dougdowson/9832019
+        const legend = svg.append("g")
+		        .attr("id", "legend")
+            .attr('fill', 'blue')
+            .attr('transform', "translate(60,30)");
+
+        const legenditem = legend.selectAll(".legenditem")
+        .data(d3.range(5))
+        .enter()
+        .append("g")
+        .attr("class", "legenditem")
+        .attr("transform", function(d, i) { return "translate(" + i * 31 + ",0)"; });
+      
+        legenditem.append("rect")
+        .attr("x", width - 240)
+        .attr("y", -7)
+        .attr("width", 30)
+        .attr("height", 8)
+        .attr("class", "rect")
+        .style("fill", function(d, i) { return legend_color[i]; });
+
+        legend.append("text")
+        .attr("x", width - 243)
+        .attr("y", 0)
+        .style("text-anchor", "end")
+        .text("Lowest SCI (-2.15)")
+        .attr('fill', 'black');
+
+        legend.append("text")
+        .attr("x", width+8)
+        .attr("y", 0)
+        .style("text-anchor", "end")
+        .text("Highest SCI (2.08)")
+        .attr('fill', 'black');
+
     });
 
 }
